8979d3f8d1a3afdef29cf18c570dfa9b2f25b98a
Anna Kruglik <kruglik.anna@gmail.com>
2016-04-08 17:29:04 +0800

    set up the project: build tools, dependencies, launcher icon

---
+++ src/main/java/com/example/jedux/todo/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.jedux.todo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
